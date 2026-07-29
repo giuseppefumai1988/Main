10ceab49e7f25517b22ed2d6203656639c68b0a2
giuseppefumai1988 <giuseppefumai1988@gmail.com>
2026-07-29 15:06:07 +0200

    Delete astro.config.mjs

--- astro.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'astro/config';
-import tina from '@tinacms/astro/integration';
-
-// Configurazione per Sito Statico Puro adatto a Vercel e TinaCMS
-export default defineConfig({
-  output: 'static', // <-- Cambiato da 'server' a 'static'
-  integrations: [tina()]
-});
